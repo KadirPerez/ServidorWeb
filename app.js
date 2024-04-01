b0237ab937662d89e224dec2b1370eb1d61b0242
Kadir Perez <perez.kadir@uabc.edu.mx>
2024-03-31 22:38:14 -0700

    CROS agregado

--- app.js
+++ app.js
@@ -1,34 +1,49 @@
-// Importa el módulo Express para crear la aplicación
-const express = require('express');
+// Se importan las librerías necesarias
+const express = require('express'); // Express para crear el servidor
+const app = express(); // Se crea la aplicación Express
+const cors = require('cors'); // Middleware para permitir CORS
+const fs = require("fs"); // Librería para manejar archivos del sistema
+const https = require("https"); // Módulo para crear servidores HTTPS
 
-// Crea una instancia de la aplicación Express
-const app = express();
+// Se define el puerto en el que escuchará el servidor
+process.env.port = 4000;
 
-// Define el puerto en el que se ejecutará el servidor
-const port = 4000;
+// Se leen los archivos de la clave privada y el certificado para HTTPS
+const llavePrivada = fs.readFileSync("./clave.key");
+const certificado = fs.readFileSync("./certificado.crt");
 
-// Importa los enrutadores para activos, ubicaciones y responsables
+// Se crean las credenciales para el servidor HTTPS
+const credenciales = {
+    key: llavePrivada,
+    cert: certificado,
+    passphrase: "Kadir" // Contraseña de la llave privada usada en la creación del certificado
+};
+
+// Se crea el servidor HTTPS utilizando las credenciales y la aplicación Express
+const httpsServer = https.createServer(credenciales, app);
+
+// Se importan los enrutadores para diferentes rutas
 const activosRouter = require('./rutas/activosRouter');
 const ubicacionesRouter = require('./rutas/ubicacionesRouter');
 const responsablesRouter = require('./rutas/responsablesRouter');
 
-// Middleware para analizar el cuerpo de las solicitudes como JSON
-app.use(express.json());
+// Se agregan middlewares a la aplicación Express
+app.use(cors()); // Middleware para permitir CORS
+app.use(express.json()); // Middleware para parsear JSON en las solicitudes
 
-// Ruta de inicio del servidor
+// Ruta de inicio para comprobar el funcionamiento del servidor
 app.get('/', (req, res) => {
     res.send('Servidor gestor de activos');
 });
 
-// Asocia los enrutadores a rutas específicas
+// Se asignan los enrutadores a diferentes rutas
 app.use('/activos', activosRouter);
 app.use('/ubicaciones', ubicacionesRouter);
 app.use('/responsables', responsablesRouter);
 
-// Inicia el servidor y escucha las solicitudes en el puerto especificado
-app.listen(port, () => {
-    console.log('Servidor escuchando por el puerto:', port);
+// El servidor HTTPS comienza a escuchar en el puerto definido
+httpsServer.listen(process.env.port, () => {
+    console.log('Servidor https escuchando por el puerto:', process.env.port);
 }).on('error', err => {
     console.log('Error al iniciar el servidor:', err);
 });
-
